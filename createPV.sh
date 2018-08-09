c005419ad29d4b4cc755bc3e07710f9392d3c6b7
oovn <41069034+oovn@users.noreply.github.com>
2018-08-10 03:01:40 +0700

    Update createPV.sh

--- createPV.sh
+++ createPV.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for i in `seq 100 999`;
+for i in `seq 10 99`;
 do
 	DIRNAME="vol$i"
 	mkdir -p /mnt/data/$DIRNAME
         chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
 	chmod 777 /mnt/data/$DIRNAME
-	sleep 1
+	#sleep 1
 	sed "s/name: vol/name: vol$i/g" vol.yaml > oc_vol.yaml
 	sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" oc_vol.yaml
 	oc create -f oc_vol.yaml
