01e3ce3e8c9dc9e2810f6fa36941d1c7b58a6799
oovn <41069034+oovn@users.noreply.github.com>
2018-08-10 21:34:11 +0700

    Update createSSL.sh

--- createSSL.sh
+++ createSSL.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/certbot/certbot 
-certbot/certbot-auto certonly --standalone --agree-tos --email binhbat@live.com -d oocp.lamit.win
-certbot/certbot-auto certonly --webroot --agree-tos --email binhbat@live.com -d okd.lamit.win
+certbot/certbot-auto certonly -d oocp.lamit.win
+certbot/certbot-auto certonly -d okd.lamit.win
