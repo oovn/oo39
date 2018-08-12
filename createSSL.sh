b842681d545c3b6e6d8e4ffb793eca8b221c5728
oovn <41069034+oovn@users.noreply.github.com>
2018-08-12 17:35:04 +0700

    Update createSSL.sh

--- createSSL.sh
+++ createSSL.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+git clone https://github.com/letsencrypt/letsencrypt
+letsencrypt/letsencrypt-auto certonly  --domains *.okd.lamit.win --email okd@lamit.win --manual --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory
+ --- or ---
 git clone https://github.com/certbot/certbot 
 certbot/certbot-auto certonly -d oocp.lamit.win
 certbot/certbot-auto certonly -d okd.lamit.win
