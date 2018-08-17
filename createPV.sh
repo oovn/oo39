87bc76e7e6d515e659c0aab4f7114a6f7acbb4fa
oovn <41069034+oovn@users.noreply.github.com>
2018-08-17 12:09:40 +0700

    Update createPV.sh

--- createPV.sh
+++ createPV.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+uuid=$(uuidgen)
 for i in `seq 10 99`;
 do
 	DIRNAME="vol$i"
-	mkdir -p /mnt/data/$DIRNAME
-        chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
+	mkdir -p /mnt/local-storage/hdd/$DIRNAME
+        chcon -Rt svirt_sandbox_file_t /mnt/local-storage/hdd/$DIRNAME
 	chmod 777 /mnt/data/$DIRNAME
 	#sleep 1
 	sed "s/name: vol/name: vol$i/g" vol.yaml > oc_vol.yaml
-	sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" oc_vol.yaml
+	sed -i "s/path: \/mnt\/local-storage\/hdd\/vol/path: \/mnt\/local-storage\/hdd\/vol$i/g" oc_vol.yaml
 	oc create -f oc_vol.yaml
 	echo "created volume $i"
 done
